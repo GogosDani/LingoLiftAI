4e9146d8dfb03d3504d977912243b2c8492b1c8c
GogosDani <gogosdaniel04@gmail.com>
2025-03-11 11:27:51 +0100

    Clean up program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -15,40 +15,13 @@ var validAudience = builder.Configuration["ValidAudience"];
 var issuerSigningKey = builder.Configuration["JwtSecretKey"];
 var frontendUrl = builder.Configuration["FrontendUrl"];
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<LingoLiftContext>(options =>
-{
-    options.UseSqlServer(connectionString);
-});
-builder.Services.AddDbContext<UsersContext>(options =>
-{
-    options.UseSqlServer(connectionString);
-});
-
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters()
-        {
-            ClockSkew = TimeSpan.Zero,
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = validIssuer,
-            ValidAudience = validAudience,
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(issuerSigningKey)
-            ),
-        };
-    });
+AddServices();
+AddDbContext();
+AddAuthentication();
+AddCors();
 
 var app = builder.Build();
-
+RunMigration();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -56,11 +29,86 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
-
 app.UseAuthentication();
+
+
+
+void AddServices()
+{
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
+
+void AddDbContext()
+{
+    builder.Services.AddDbContext<LingoLiftContext>(options =>
+    {
+        options.UseSqlServer(connectionString);
+    });
+    builder.Services.AddDbContext<UsersContext>(options =>
+    {
+        options.UseSqlServer(connectionString);
+    });
+}
+
+void AddAuthentication()
+{
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = validIssuer,
+                ValidAudience = validAudience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(issuerSigningKey)
+                ),
+            };
+        });
+}
+
+void RunMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var solarDb = scope.ServiceProvider.GetRequiredService<LingoLiftContext>();
+        var usersDb = scope.ServiceProvider.GetRequiredService<UsersContext>();
+        if (solarDb.Database.ProviderName != "Microsoft.EntityFrameworkCore.InMemory")
+        {
+            if (solarDb.Database.GetPendingMigrations().Any())
+            {
+                solarDb.Database.Migrate();
+            }
+        }
+                
+        if (usersDb.Database.ProviderName != "Microsoft.EntityFrameworkCore.InMemory")
+        {
+            if (usersDb.Database.GetPendingMigrations().Any())
+            {
+                usersDb.Database.Migrate();
+            }
+        }
+    } 
+}
+
+void AddCors()
+{
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowFrontend",
+            builder => builder
+                .WithOrigins(frontendUrl)
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+    });
+}
