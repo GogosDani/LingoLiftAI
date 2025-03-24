9cd46f838ca96b7d9250dcf4fb30ae62fa13b31b
GogosDani <gogosdaniel04@gmail.com>
2025-03-24 18:30:00 +0100

    FE: choose language page

--- Frontend/vite-project/src/Components/ChooseNewLanguage.tsx
+++ Frontend/vite-project/src/Components/ChooseNewLanguage.tsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react"
 import { api } from "../axios/api";
+import Headbar from "./Headbar";
+import LoginForm from "../Components/LoginForm";
+import RegisterForm from "../Components/RegisterForm";
+
 
 type Language = {
     languageName: string;
@@ -10,6 +14,8 @@ type Language = {
 export default function ChooseNewLanguage() {
 
     const [languages, setLanguages] = useState<Language[]>([]);
+    const [showRegisterForm, setShowRegisterForm] = useState(false);
+    const [showLoginForm, setShowLoginForm] = useState(false);
 
     useEffect(() => {
         async function fetchLanguages() {
@@ -21,11 +27,26 @@ export default function ChooseNewLanguage() {
 
     return (
         <>
-            <div> Which language do you want to learn? </div>
-            {languages.map(l => <div key={l.id}>
-                <img src={l.flag} className="w-4" />
-                <div> {l.languageName}  </div>
-            </div>)}
+            <Headbar showRegisterForm={setShowRegisterForm} showLoginForm={setShowLoginForm} />
+            <div className="flex flex-col items-center justify-center h-screen text-center gap-36">
+                <div className="font-mono text-6xl font-bold">Which language do you want to learn?</div>
+                <div className="flex flex-row gap-4">
+                    {languages.map(l => (
+                        <div
+                            key={l.id}
+                            className="flex flex-col items-center w-32 transition-transform duration-300 hover:scale-110"
+                        >
+                            <div className="w-24 h-20">
+                                <img src={l.flag} />
+                            </div>
+                            <div className="font-bold text-2xl">{l.languageName}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            {showLoginForm && <LoginForm show={setShowLoginForm} />}
+            {showRegisterForm && <RegisterForm show={setShowRegisterForm} />}
         </>
+
     )
 }
