b8e9e0bfe91fec7f3f170ec3cdd9b467f9b2a3a6
GogosDani <gogosdaniel04@gmail.com>
2025-04-06 10:22:17 +0200

    logout controller method

--- Frontend/vite-project/src/Components/Headbar.tsx
+++ Frontend/vite-project/src/Components/Headbar.tsx
@@ -22,6 +22,14 @@ export default function Headbar({ showRegisterForm, showLoginForm }: HeaderProps
         checkIfUserLoggedIn();
     }, [])
 
+    async function handleLogout() {
+        try {
+            await api.post('/api/auth/logout');
+        } catch (error) {
+            console.error('Logout failed:', error);
+        }
+    }
+
     return (
         <div className="fixed top-0 w-full border-b-2 flex flex-row h-12 px-4 md:px-20 justify-between bg-white z-10">
             <div className="flex flex-row gap-4 md:gap-8">
@@ -30,7 +38,10 @@ export default function Headbar({ showRegisterForm, showLoginForm }: HeaderProps
                 <button className="text-sm md:text-lg" onClick={() => navigate("/leaderboard")}> Leaderboard </button>
             </div>
             {loggedIn ? (
-                <img src="/profile.png" className="w-8 h-8 self-center" />
+                <div className="flex flex-row gap-4 md:gap-8 justify-center">
+                    <img src="/profile.png" className="w-6 h-6 self-center" />
+                    <button onClick={() => handleLogout()} className="self-center bg-blue-600 rounded-xl h-3/4 w-20 md:w-24 text-white font-bold"> LOG OUT </button>
+                </div>
             ) : (
                 <div className="flex flex-row gap-4 md:gap-8 justify-center">
                     <button onClick={() => showLoginForm(true)} className="self-center bg-blue-600 rounded-xl h-3/4 w-20 md:w-24 text-white font-bold"> Log in </button>
