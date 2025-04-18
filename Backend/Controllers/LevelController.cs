3f609acf67d208fccc2bb52e5f41fd6d2d3f045c
GogosDani <gogosdaniel04@gmail.com>
2025-04-18 10:24:11 +0200

    get users level endpoint

--- Backend/Controllers/LevelController.cs
+++ Backend/Controllers/LevelController.cs
@@ -1,4 +1,7 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Backend.Services.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers;
@@ -11,5 +14,23 @@ public class LevelController : ControllerBase
     public LevelController(ILevelRepository repository)
     {
         _repository = repository;
+    }
+    
+    [HttpGet("{languageId}"), Authorize]
+    public async Task<ActionResult> GetUserLevel(int languageId)
+    {
+        try
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(HttpContext.Request.Cookies["jwt"]);
+            var userId = jwtToken.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            var level = _repository.GetUserLevelByLanguageId(languageId, userId);
+            return Ok(new { level = level });
+        } 
+        catch (Exception ex)
+        {
+            await Console.Error.WriteLineAsync(ex.Message);
+            return StatusCode(500, new { message = "An unexpected error occurred. Please try again later." });
+        }
     }
 }
